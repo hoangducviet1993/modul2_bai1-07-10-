c3e40c72a010b596aef913240aef374f6b38995a
hoangducviet1993 <ducviet12vl@gmail.com>
2021-10-12 10:06:11 +0700

    Bai4 (12-10)

--- bai4/fan-class/src/com/codegym/Fan.java
+++ bai4/fan-class/src/com/codegym/Fan.java
@@ -1,10 +1,53 @@
 package com.codegym;
 
 public class Fan {
-    boolean on;
-    double radius;
-    String color;
-    int Speed;
+    private int SLOW;
+    private int MEDIUM;
+    private int FAST;
+    private boolean on;
+    private double radius;
+    private String color;
+    private int speed;
 
+    public Fan() {
+        this.SLOW = 1;
+        this.MEDIUM = 2;
+        this.FAST = 3;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+        this.speed = SLOW;
 
+    }
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    public void setColor( String color){
+        this.color= color;
+    }
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public int setSpeed(int number) {
+        switch (number) {
+            case 1:
+                this.speed = SLOW;
+                break;
+            case 2:
+                this.speed = MEDIUM;
+                break;
+            case 3:
+                this.speed = FAST;
+                break;
+        }
+        return speed;
+    }
+    public String toSing(){
+        if (on){
+            return "This fan :{ speed : " + speed + " ; color : " + color + " ;radius : " + radius + " .Fan is on }" ;
+        }else {
+            return "This fan :{ color : " + color + " ;radius : " + radius + " .Fan is off }" ;
+        }
+    }
 }
--- bai4/fan-class/src/com/codegym/Main.java
+++ bai4/fan-class/src/com/codegym/Main.java
@@ -1,8 +1,24 @@
 package com.codegym;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Fan fan1 = new Fan();
+        fan1.setOn(true);
+        System.out.println("Enter speed: ");
+        int speed = scanner.nextInt();
+        fan1.setSpeed(speed);
+        System.out.println("Enter Radius: ");
+        double radius= scanner.nextDouble();
+        fan1.setRadius(radius);
+        scanner.nextLine();
+        System.out.println("Enter Color: ");
+        String color = scanner.nextLine();
+        fan1.setColor(color);
+        System.out.println(fan1.toSing());
+
     }
 }
