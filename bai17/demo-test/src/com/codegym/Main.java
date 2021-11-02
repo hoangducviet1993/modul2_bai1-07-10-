6c40812699fcccf3d25bde66a2c2cd04ca4b2529
hoangducviet1993 <ducviet12vl@gmail.com>
2021-11-02 17:30:20 +0700

    update

--- bai17/demo-test/src/com/codegym/Main.java
+++ bai17/demo-test/src/com/codegym/Main.java
@@ -6,38 +6,5 @@ class Main {
 
     public static void main(String[] args) throws IOException {
 
-//        // Tạo một luồng đầu vào bằng cách đọc một file
-//        InputStream in = new FileInputStream("C:\\Users\\vieth\\OneDrive\\Desktop\\baiThucHanh\\modul2_bai1-07-10-\\bai17\\demo-test\\src\\com\\codegym\\CodeGym.txt");
-//
-//        // Mảng để mỗi lần đọc các byte từ luồng thì tạm thời để lên đó
-//        // Ta dùng mảng 10 byte
-//
-//        byte[] bytes = new byte[10];
-//        int i = -1;
-//
-//        // Đọc các byte trong luồng và gán lên các phần tử của mảng.
-//        // Giá trị i là số đọc được của 1 lần. (i sẽ <= 10).
-//        // Khi không còn phần tử trong luồng i sẽ = -1
-//        while ((i = in.read(bytes)) != -1) {
-//            // Tạo String từ các byte đọc được
-//            String s = new String(bytes, 0, i);
-//            System.out.println(s);
-//        }
-//        in.close();
-
-        // Tạo một luồng ký tự đầu ra với mục đích ghi thông tin vào file
-        OutputStream os = new FileOutputStream("output.txt");
-
-        // Tạo một mảng byte ,ta sẽ ghi các byte này vào file nói trên .
-        byte[] bytes = new byte[] { 'C', 'o', 'd', 'e', 'G', 'y', 'm' };
-        
-        // Ghi lần lượt các ký tự vào luồng
-        for (int i = 0; i < bytes.length; i++) {
-            byte b = bytes[i];
-            // Ghi ký tự vào luồng
-            os.write(b);
-        }
-        // Đóng luồng đầu ra lại việc ghi xuống file hoàn tất.
-        os.close();
     }
 }
---
+++ bai17/demo-test/src/model/Person.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Person {
+    private String name;
+    private int age;
+    private int id = 0;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
+}
